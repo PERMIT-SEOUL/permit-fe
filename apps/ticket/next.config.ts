e84ad01c6920eb5703f2350a6237955b33b36f1a
jerry <becomegoodguy@gmail.com>
2025-06-29 17:14:25 +0900

    [ticket] feat: 요청 경로 리다이렉션 및 Axios 설정 수정

--- apps/ticket/next.config.ts
+++ apps/ticket/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    // 요청 경로를 다른 경로에 매핑
+    return [
+      {
+        source: "/sample",
+        destination: "/404",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
