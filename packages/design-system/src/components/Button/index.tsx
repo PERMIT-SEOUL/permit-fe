f4562c3e99d0439cd9586344ab34c7a7be2fae25
jerry <becomegoodguy@gmail.com>
2025-07-06 15:31:58 +0900

    [design-system] feat: 버튼 컴포넌트 추가 및 스타일 정의

--- packages/design-system/src/components/Button/index.tsx
+++ packages/design-system/src/components/Button/index.tsx
@@ -1,17 +1,74 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import classNames from "classnames/bind";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName?: string;
+import styles from "./index.module.scss";
+
+const cx = classNames.bind(styles);
+
+export type ButtonSize = "sm" | "md";
+export type ButtonVariant = "primary" | "secondary" | "cta" | "error" | "unac";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * 버튼 크기
+   * @default "md"
+   */
+  size?: ButtonSize;
+  /**
+   * 버튼 변형
+   * @default "primary"
+   */
+  variant?: ButtonVariant;
+  /**
+   * 버튼 내부 컨텐츠
+   */
+  children: React.ReactNode;
+  /**
+   * 버튼 비활성화 여부
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * 버튼 전체 너비 적용 여부
+   * @default false
+   */
+  fullWidth?: boolean;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button className={className} onClick={() => alert(`Hello from your ${appName} app!`)}>
-      {children}
-    </button>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      size = "md",
+      variant = "primary",
+      children,
+      disabled = false,
+      fullWidth = false,
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <button
+        ref={ref}
+        className={cx(
+          "button",
+          `button--${size.toLowerCase()}`,
+          `button--${variant}`,
+          {
+            "button--full-width": fullWidth,
+          },
+          className,
+        )}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
