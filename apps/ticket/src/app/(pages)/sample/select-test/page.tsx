586257c58dc2abbe766859f672ec6341e04c6789
jerry <becomegoodguy@gmail.com>
2025-07-19 17:12:37 +0900

    [ticket] feat: Select 컴포넌트 테스트 페이지 추가
    - Select 컴포넌트 사용 예제 페이지 구현
    - 단일/다중 선택 테스트 케이스 추가

---
+++ apps/ticket/src/app/(pages)/sample/select-test/page.tsx
@@ -0,0 +1,10 @@
+import { SelectExample } from "./_clientBoundary/SelectExample";
+
+export default function SelectTestPage() {
+  return (
+    <div style={{ padding: "20px", minHeight: "100vh" }}>
+      <h1 style={{ color: "white", marginBottom: "30px" }}>Select Component Test</h1>
+      <SelectExample />
+    </div>
+  );
+}
