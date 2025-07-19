9e966e3802790594fdddba401c1cd7162136f918
jerry <becomegoodguy@gmail.com>
2025-07-20 01:42:33 +0900

    [ticket] feat: 이벤트 페이지 레이아웃 및 mock 데이터 업데이트

--- apps/ticket/src/app/(pages)/event/[eventId]/constants/mock.ts
+++ apps/ticket/src/app/(pages)/event/[eventId]/constants/mock.ts
@@ -1,28 +1,51 @@
 // TODO: mock data 수정
 export const mockEvent = {
-  eventName: "Summer Vibes Festival",
-  venue: "Seoul Olympic Stadium",
+  eventName: "Ceiling service vol.6 - Ksawery Komputery [PL]",
+  venue: "TBA - Layer 41, 성수동",
   date: "Sun-mon, 25-26 May 2025",
-  time: "18:00 - 22:00",
-  minAge: 19,
-  details: "한여름 밤을 뜨겁게 달굴 뮤직 페스티벌!",
+  time: "19:00 - 22:00",
+  minAge: 18,
+  details: `PERMIT이 선보이는 ceiling service vol.6은 성수동 Layer 41에서 펼쳐지는 예술과 사운드의 몰입형 퍼포먼스입니다. Ksawery Komputery(폴란드)를 비롯해 Lewis OfMan, CIFIKA, Danny Arens, Gazaebal, Hahim, sooom, Yeesangyu가 참여하며, 박유석 작가의 라이브 비주얼이 함께합니다.
+
+명상으로 시작해 연주와 설치로 이어지는 시간 속에서, 일상의 흐름을 멈추고 깊이 듣는 감각을 회복해보세요. 고요한 몰입의 여정에 여러분을 초대합니다.PERMIT이 선보이는 ceiling service vol.6은 성수동 Layer 41에서 펼쳐지는 예술과 사운드의 몰입형 퍼포먼스입니다. Ksawery Komputery(폴란드)를 비롯해 Lewis OfMan, CIFIKA, Danny Arens, Gazaebal, Hahim, sooom, Yeesangyu가 참여하며, 박유석 작가의 라이브 비주얼이 함께합니다.
+
+명상으로 시작해 연주와 설치로 이어지는 시간 속에서, 일상의 흐름을 멈추고 깊이 듣는 감각을 회복해보세요. 고요한 몰입의 여정에 여러분을 초대합니다.`,
   lineup: [
     {
-      category: "[Headliner]",
-      artists: [{ name: "NewJeans" }, { name: "Zico" }],
+      category: "[Installation & Visuals]",
+      artists: [{ name: "Ksawery Komputery" }],
     },
     {
-      category: "[Opening Act]",
-      artists: [{ name: "LUCY" }, { name: "10CM" }],
+      category: "[PL]",
+      artists: [{ name: "Yuseok Bak" }],
+    },
+    {
+      category: "[Ambient live set]",
+      artists: [
+        { name: "Lewis OfMan (Ambient Set)" },
+        { name: "CIFIKA & UMAKA" },
+        { name: "Haihm" },
+        { name: "Danny Arens & gazaebal" },
+        { name: "Yeesangyu & sooom" },
+      ],
+    },
+    {
+      category: "[Visual Artist]",
+      artists: [{ name: "Yuseok Bak" }],
     },
   ],
   images: [
     {
-      imageUrl:
-        "https://scontent-icn2-1.cdninstagram.com/v/t51.2885-15/500334637_18065689073489590_1168903891502206281_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjEwODB4MTM1MC5zZHIuZjc1NzYxLmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=scontent-icn2-1.cdninstagram.com&_nc_cat=110&_nc_oc=Q6cZ2QGahG54F3OXjEzOlGnkcmtxfVQ7GazeDgSR-bfiRVswhmICcm4RS_zUemoy9alKu7Y&_nc_ohc=3q3CofWZ9TIQ7kNvwG9U3ZF&_nc_gid=DTO0oxJbBQR8N_vg_UrL1Q&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzYzOTQ0NjE5MTU4MDUzNjM2Mg%3D%3D.3-ccb7-5&oh=00_AfQa62J13gyKCd5sFXTPxb6cE_zsDaeF6hhHyyBnNSlYTQ&oe=6880F422&_nc_sid=7a9f4b",
+      imageUrl: "https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?w=500&h=600&fit=crop",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/70939232?v=4",
+      imageUrl: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=500&h=600&fit=crop",
+    },
+    {
+      imageUrl: "https://images.unsplash.com/photo-1501386761578-eac5c94b800a?w=500&h=600&fit=crop",
+    },
+    {
+      imageUrl: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?w=500&h=600&fit=crop",
     },
   ],
 };
--- apps/ticket/src/app/(pages)/event/[eventId]/page.tsx
+++ apps/ticket/src/app/(pages)/event/[eventId]/page.tsx
@@ -1,9 +1,12 @@
-import { Flex } from "@permit/design-system";
+import classNames from "classnames/bind";
 
 import { FloatingSection } from "./_clientBoundray/FloatingSection";
 import { ImageCarouselClient } from "./_clientBoundray/ImageCarouselClient";
 import { EventInfo } from "./_components/EventInfo";
 import { mockEvent } from "./constants/mock";
+import styles from "./index.module.scss";
+
+const cx = classNames.bind(styles);
 
 type Props = {
   params: Promise<{ eventId: string }>;
@@ -19,18 +22,37 @@ const EventDetailPage = async ({ params }: Props) => {
 
   return (
     <>
-      <Flex>
+      <div className={cx("content")}>
+        <div className={cx("mobile_title_section")}>
+          <EventInfo
+            eventName={mockEvent.eventName}
+            venue={mockEvent.venue}
+            date={mockEvent.date}
+            time={mockEvent.time}
+            minAge={mockEvent.minAge}
+            details={mockEvent.details}
+            lineup={mockEvent.lineup}
+            showOnlyTitle={true}
+          />
+        </div>
+
+        {/* <div className={cx("image_section")}> */}
         <ImageCarouselClient images={mockEvent.images} />
-        <EventInfo
-          eventName={mockEvent.eventName}
-          venue={mockEvent.venue}
-          date={mockEvent.date}
-          time={mockEvent.time}
-          minAge={mockEvent.minAge}
-          details={mockEvent.details}
-          lineup={mockEvent.lineup}
-        />
-      </Flex>
+        {/* </div> */}
+
+        <div className={cx("info_section")}>
+          <EventInfo
+            eventName={mockEvent.eventName}
+            venue={mockEvent.venue}
+            date={mockEvent.date}
+            time={mockEvent.time}
+            minAge={mockEvent.minAge}
+            details={mockEvent.details}
+            lineup={mockEvent.lineup}
+            showOnlyTitle={false}
+          />
+        </div>
+      </div>
 
       <FloatingSection eventId={Number(eventId)} />
     </>
