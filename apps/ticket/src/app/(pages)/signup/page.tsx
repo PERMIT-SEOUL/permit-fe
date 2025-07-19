8e3fc6b417af142ea9f9888c6487d78eee21d6f9
jerry <becomegoodguy@gmail.com>
2025-07-19 18:43:47 +0900

    [ticket[ chore: 불필요 import 제거

--- apps/ticket/src/app/(pages)/signup/page.tsx
+++ apps/ticket/src/app/(pages)/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import classNames from "classnames/bind";
 
