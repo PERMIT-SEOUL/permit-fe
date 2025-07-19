525a312c6751be5c350f246fd40e8c2a35c1f62c
jerry <becomegoodguy@gmail.com>
2025-07-19 17:28:00 +0900

    [design-system] fix: 날짜 형식 변경 및 onChange 함수 수정

--- packages/design-system/src/components/Select/index.tsx
+++ packages/design-system/src/components/Select/index.tsx
@@ -75,7 +75,12 @@ export const Select = ({
 
   const handleDateSelect = (date: Date) => {
     setSelectedDate(date);
-    onChange(date.toISOString().split("T")[0] ?? "");
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    onChange(`${year}-${month}-${day}`);
     setIsOpen(false);
   };
 
