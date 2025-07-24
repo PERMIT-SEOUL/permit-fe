f398aa9af91522730d02bef8beb40c4af9cf78af
jerry <becomegoodguy@gmail.com>
2025-07-24 09:21:09 +0900

    [ticket] chore: 목 데이터 이벤트 details 변경

--- apps/ticket/src/app/(pages)/event/[eventId]/constants/mock.ts
+++ apps/ticket/src/app/(pages)/event/[eventId]/constants/mock.ts
@@ -7,7 +7,7 @@ export const mockEvent = {
   minAge: 18,
   details: `PERMIT이 선보이는 ceiling service vol.2은 성수동 Layer 41에서 펼쳐지는 예술과 사운드의 몰입형 퍼포먼스입니다. Ksawery Komputery(폴란드)를 비롯해 Lewis OfMan, CIFIKA, Danny Arens, Gazaebal, Hahim, sooom, Yeesangyu가 참여하며, 박유석 작가의 라이브 비주얼이 함께합니다.
 
-명상으로 시작해 연주와 설치로 이어지는 시간 속에서, 일상의 흐름을 멈추고 깊이 듣는 감각을 회복해보세요. 고요한 몰입의 여정에 여러분을 초대합니다.PERMIT이 선보이는 ceiling service vol.6은 성수동 Layer 41에서 펼쳐지는 예술과 사운드의 몰입형 퍼포먼스입니다. Ksawery Komputery(폴란드)를 비롯해 Lewis OfMan, CIFIKA, Danny Arens, Gazaebal, Hahim, sooom, Yeesangyu가 참여하며, 박유석 작가의 라이브 비주얼이 함께합니다.
+명상으로 시작해 연주와 설치로 이어지는 시간 속에서, 일상의 흐름을 멈추고 깊이 듣는 감각을 회복해보세요. 고요한 몰입의 여정에 여러분을 초대합니다.PERMIT이 선보이는 ceiling service vol.2은 성수동 Layer 41에서 펼쳐지는 예술과 사운드의 몰입형 퍼포먼스입니다. Ksawery Komputery(폴란드)를 비롯해 Lewis OfMan, CIFIKA, Danny Arens, Gazaebal, Hahim, sooom, Yeesangyu가 참여하며, 박유석 작가의 라이브 비주얼이 함께합니다.
 
 명상으로 시작해 연주와 설치로 이어지는 시간 속에서, 일상의 흐름을 멈추고 깊이 듣는 감각을 회복해보세요. 고요한 몰입의 여정에 여러분을 초대합니다.`,
   lineup: [
