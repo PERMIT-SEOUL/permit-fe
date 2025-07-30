c975f5ed90dc5e88f68b96827a9e5ad896bd79a4
jerry <becomegoodguy@gmail.com>
2025-07-30 23:29:58 +0900

    [ticket] fix: 마이페이지 prerender 오류 해결
    - dynamic = 'force-dynamic' 설정 추가
    - 사용자별 데이터를 보여주는 페이지이므로 동적 렌더링으로 변경

--- apps/ticket/src/app/(pages)/mypage/page.tsx
+++ apps/ticket/src/app/(pages)/mypage/page.tsx
@@ -5,6 +5,9 @@ import styles from "./index.module.scss";
 
 const cx = classNames.bind(styles);
 
+// 마이페이지는 사용자별 데이터를 보여주므로 동적 렌더링으로 설정
+export const dynamic = "force-dynamic";
+
 /**
  * 마이페이지
  */
