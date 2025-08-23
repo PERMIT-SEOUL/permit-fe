4567bd35600397ff0aa5ccb979ace9d39ff93cb4
jerry <becomegoodguy@gmail.com>
2025-08-24 04:12:47 +0900

    [ticket] feat: 주문 처리 중 페이지 추가

---
+++ apps/ticket/src/app/(pages)/order/[orderId]/process/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { usePaymentConfirmMutation } from "@/data/payments/postPaymentConfirm/mutation";
+import { LoadingWithLayout } from "@/shared/components/LoadingWithLayout";
+
+/**
+ * 결제 요청 페이지
+ */
+const PaymentProcessPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { mutateAsync } = usePaymentConfirmMutation();
+
+  useEffect(() => {
+    const handlePaymentConfirm = async () => {
+      try {
+        const data = await mutateAsync({
+          paymentKey: searchParams.get("paymentKey") || "",
+          orderId: searchParams.get("orderId") || "",
+          totalAmount: Number(searchParams.get("amount")) || 0,
+        });
+
+        const { eventName, eventDate } = data;
+
+        window.location.replace(
+          `${window.location.origin}/order/${searchParams.get("orderId")}/success?eventName=${eventName}&eventDate=${eventDate}`,
+        );
+      } catch (error) {
+        // TODO: 에러 처리 로직 추가
+        // 에러 메시지 세분화 요청
+        // 결제 실패 페이지로 라우팅
+        alert("결제 실패");
+
+        window.location.replace(
+          `${window.location.origin}/order/${searchParams.get("orderId")}/fail`,
+        );
+      }
+    };
+
+    handlePaymentConfirm();
+  }, [mutateAsync, searchParams, router]);
+
+  if (isLoading) {
+    return <LoadingWithLayout />;
+  }
+};
+
+export default PaymentProcessPage;
