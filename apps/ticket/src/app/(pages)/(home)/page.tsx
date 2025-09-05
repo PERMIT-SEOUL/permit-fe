76e06d28b3ac614abf457e8ff1aa74fb926848be
jerry <becomegoodguy@gmail.com>
2025-09-05 20:40:45 +0900

    fix: EVENT_QUERY_KEYS 관련 코드 제거 및 불필요한 로그 삭제

--- apps/ticket/src/app/(pages)/(home)/page.tsx
+++ apps/ticket/src/app/(pages)/(home)/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { eventsOptions } from "@/data/events/getEvents/queries";
-import { EVENT_QUERY_KEYS } from "@/data/events/queryKeys";
 import { getQueryClient } from "@/lib/queryClient/helpers/getQueryClient";
 import { LoadingWithLayout } from "@/shared/components/LoadingWithLayout";
 
@@ -13,8 +12,6 @@ const HomePage = async () => {
 
   // TODO: 스트리밍 되어서 내려오는 게 에러... 바운더리?
   await qc.fetchQuery(eventsOptions());
-
-  console.log(qc.getQueryData([EVENT_QUERY_KEYS.LIST]));
 
   return (
     <HydrationBoundary state={dehydrate(qc)}>
