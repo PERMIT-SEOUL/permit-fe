09fdfa3dc2f1dd0bc761c7f6dcef544b86825f07
jerry <becomegoodguy@gmail.com>
2025-09-08 22:54:18 +0900

    [ticket-admin] fix: QueryClientProviders 컴포넌트의 props 타입 정의 수정

--- apps/ticket-admin/src/lib/queryClient/clientBoundary/QueryClientProvider/index.tsx
+++ apps/ticket-admin/src/lib/queryClient/clientBoundary/QueryClientProvider/index.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import type { PropsWithChildren } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { getQueryClient } from "../../helpers/getQueryClient";
 
-export const QueryClientProviders = ({ children }: PropsWithChildren) => {
+export const QueryClientProviders = ({ children }: { children: React.ReactNode }) => {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
   //       suspend because React will throw away the client on the initial
