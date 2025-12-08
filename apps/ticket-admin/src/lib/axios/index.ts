a0eb6e4a054257ea9f5d7da94dda1410204a080a
jerry <becomegoodguy@gmail.com>
2025-12-08 23:44:25 +0900

    fix: errorCode 수정

--- apps/ticket-admin/src/lib/axios/index.ts
+++ apps/ticket-admin/src/lib/axios/index.ts
@@ -48,6 +48,18 @@ instance.interceptors.response.use(
     }
 
     if (isAxiosErrorResponse(error.response?.data)) {
+      // 엑세스 토큰 없음
+      if (error.response?.data.code === ERROR_CODE.NO_ACCESS_TOKEN) {
+        alert("로그인이 필요한 페이지입니다.");
+
+        // 엑세스 토큰 재발급 실패시 로그인 페이지로 이동
+        safeLocalStorage.remove(IS_LOGINED);
+
+        window.location.href = EXTERNAL_PATH.LOGIN;
+
+        return;
+      }
+
       // 액세스 토큰 만료
       if (error.response?.data.code === ERROR_CODE.ACCESS_TOKEN_EXPIRED) {
         try {
--- apps/ticket/src/lib/axios/index.ts
+++ apps/ticket/src/lib/axios/index.ts
@@ -50,6 +50,16 @@ instance.interceptors.response.use(
 
     // 로그인이 필요한 요청이거나, 리프레시 토큰 모두 만료시 로그인 페이지로 이동
     if (isAxiosErrorResponse(error.response?.data)) {
+      // 엑세스 토큰 없음
+      if (error.response?.data.code === ERROR_CODE.NO_ACCESS_TOKEN) {
+        alert("로그인이 필요한 페이지입니다.");
+
+        safeLocalStorage.remove(IS_LOGINED);
+        window.location.href = "/login";
+
+        return;
+      }
+
       if (error.response?.data.code === ERROR_CODE.LOGIN_REQUIRED) {
         // 인증 페이지에서는 로그인 페이지로 이동하지 않음
         if (window.location.pathname !== "/auth") {
