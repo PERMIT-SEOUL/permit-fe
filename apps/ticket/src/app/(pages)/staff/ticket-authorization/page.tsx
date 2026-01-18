183812134d82129b41d6b38080b0c163b65c2648
jerry <becomegoodguy@gmail.com>
2026-01-18 22:20:02 +0900

    feat: add Header component to Ticket Authorization page and update layout styles

--- apps/ticket/src/app/(pages)/staff/ticket-authorization/page.tsx
+++ apps/ticket/src/app/(pages)/staff/ticket-authorization/page.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./_clientBoundary/Header";
 import { TicketAuthorizationClient } from "./_clientBoundary/TicketAuthorizationClient";
 
 const TicketAuthorizationPage = () => {
-  return <TicketAuthorizationClient />;
+  return (
+    <>
+      <Header />
+      <TicketAuthorizationClient />
+    </>
+  );
 };
 
 export default TicketAuthorizationPage;
