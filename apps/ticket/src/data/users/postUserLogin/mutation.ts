cca59fbb91c76f144343f67a0568f7783835e5f4
jerry <becomegoodguy@gmail.com>
2026-01-19 22:12:11 +0900

    refactor: clean up unused code in postUserLogin mutation file

--- apps/ticket/src/data/users/postUserLogin/mutation.ts
+++ apps/ticket/src/data/users/postUserLogin/mutation.ts
@@ -1,41 +1,5 @@
-// import { useMutation, UseMutationOptions } from "@tanstack/react-query";
-
-// import { API_URL } from "@/data/constants";
-// import { instance } from "@/lib/axios";
-// import { SocialLoginType } from "@/shared/hooks/useOAuth/types";
-
-// type LoginRequest = {
-//   socialType: SocialLoginType;
-//   authorizationCode: string;
-//   redirectUrl: string;
-// };
-
-// type LoginResponse = {
-//   data: null;
-// };
-
-// export type LoginMutationOptions<TData> = Omit<
-//   UseMutationOptions<TData, Error, LoginRequest>,
-//   "mutationFn"
-// > & {
-//   onSuccess?: (data: TData) => void;
-// };
-
-// export const useLoginMutation = (options?: LoginMutationOptions<LoginResponse>) => {
-//   return useMutation({
-//     mutationFn: async (params: LoginRequest) => {
-//       const { data } = await instance.post<LoginResponse>(API_URL.USER.LOGIN, params);
-
-//       return data;
-//     },
-//     ...options,
-//   });
-// };
-
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 
-import { API_URL } from "@/data/constants";
-import { instance } from "@/lib/axios";
 import { SocialLoginType } from "@/shared/hooks/useOAuth/types";
 
 type LoginRequest = {
